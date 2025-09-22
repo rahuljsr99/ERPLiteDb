ebe59651a352b2c121f9e11799f8827530ede99e
Rahul Kumar <rahulkumar.jsr130299@gmail.com>
2025-09-22 22:28:29 +0530

    Add project files.

---
+++ ERPLiteDb/dbo/Tables/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]      INT            IDENTITY (1, 1) NOT NULL,
+    [UserName]    NVARCHAR (100) NULL,
+    [Password]    NVARCHAR (100) NULL,
+    [PhoneNumber] BIGINT         NULL,
+    [Email]       NVARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
